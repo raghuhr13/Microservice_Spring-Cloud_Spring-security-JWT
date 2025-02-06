96f7db903faf01d66174423af1ae8c01c23a8b65
raghuhr13 <125434095+raghuhr13@users.noreply.github.com>
2025-02-06 15:51:31 +0530

    Spring Cloud Microservice - Naming Server
    It can also be called as Discovery server

---
+++ SCMS-naming-server/src/main/java/com/hr/scms/ScmsNamingServerApplication.java
@@ -0,0 +1,25 @@
+package com.hr.scms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ScmsNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScmsNamingServerApplication.class, args);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ SCMS-naming-server/src/test/java/com/hr/scms/ScmsNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.scms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmsNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
