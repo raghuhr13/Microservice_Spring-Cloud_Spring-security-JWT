208ac4e31a1b097ebf7eb500bf5e5d6858448faa
raghuhr13 <125434095+raghuhr13@users.noreply.github.com>
2025-02-06 16:34:26 +0530

    Spring Cloud Config Server - Config Server
    It's not a part of Spring Cloud Microservice, It will help to fetch configurations from the defined Source, these configurations are used in Spring Cloud Microservice - Limit Service

---
+++ SCMS-cloud-config-server/src/main/java/com/hr/scms/ScmsCloudConfigServerApplication.java
@@ -0,0 +1,29 @@
+package com.hr.scms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ScmsCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScmsCloudConfigServerApplication.class, args);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
