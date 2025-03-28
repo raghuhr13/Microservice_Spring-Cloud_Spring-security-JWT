9896aa7acea3e42c707cd6a8ad1e145c80aef5eb
raghuhr13 <125434095+raghuhr13@users.noreply.github.com>
2025-03-28 14:09:07 +0530

    Added Redis Message Broker Publish/Subscribe

--- SCMS-second-microservice/src/main/java/com/hr/scms/controller/ScmsSecondMsController.java
+++ SCMS-second-microservice/src/main/java/com/hr/scms/controller/ScmsSecondMsController.java
@@ -1,17 +1,30 @@
 package com.hr.scms.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hr.scms.service.RedisMessagePublisher;
 
 @RestController
 @RequestMapping("/SCMS-second-microservice")
 public class ScmsSecondMsController {
 	
+	@Autowired
+	private RedisMessagePublisher redisMessagePublishing;
+	
+	
+	@PostMapping("/postA")
+	public void sendMess(@RequestParam String channel, @RequestParam String message) {
+		redisMessagePublishing.publish(channel, message);
+	}
+	
 	@GetMapping("/test-1")
 	public String data() {
 		return "Data From Second Micro Service";
 	}
-
 }
 
